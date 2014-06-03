9ae4ea721797250db847426ae59faf1d3cc0e68e
Fabio Poloni <fabio@APP-roved.com>
2014-06-03 10:53:15 +0200

    moved the folder-reading mechanism to its own place

--- index.php
+++ index.php
@@ -1,46 +1,6 @@
 <?php
-	include "include/header.php" 
-?>
-
-<?php
-
-// order for the structuring levels
-$order = array('y', 'm', 'd', 'h', 'i');
-
-// here will the sorted levels be
-$content = array('y' => array(), 'm' => array(), 'd' => array(), 'h' => array(), 'i' => array());
-
-// months-names
-$months = array('Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember');
-
-// sort the directory
-function sortDir($dir, $level = 0) {
-    global $order, $content;
-    $dirhandle = opendir($dir);
-    while (($file = readdir($dirhandle))) {
-        
-        // still not agreeing that this is necessary
-        if ($file == '.' || $file == '..') {
-            continue;
-        }
-        
-        // if it's a directory let's move on an sort it into the structure above
-        if (is_dir($dir . '/' . $file)) {
-            
-            // if it's not already an array then create one
-            if (!in_array($file, $content[$order[$level]])) {
-                $content[$order[$level]][] = $file;
-            }
-            
-            // recursion!
-            sortDir($dir . '/' . $file, $level + 1);
-        }
-    }
-}
-
-// entry point for pics-folder
-sortDir('pics');
-
+	include "include/header.php";
+    include "lib/sort-pictures.php";
 ?>
 
 <div class="container">
