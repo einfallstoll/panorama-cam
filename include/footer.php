1000d4ae7e1cfe34fe1563ea83f0828b04594f4b
Fabio Poloni <contact@APP-roved.com>
2014-06-03 10:29:16 +0200

    remove blueimp because it's worthless

--- include/footer.php
+++ include/footer.php
@@ -4,16 +4,5 @@
 				<p class="text-muted credit">Site courtesy Fabio Poloni & Florian Siffer</p>
 			</div>
 		</div>
-		<script src="js/blueimp-gallery.min.js"></script>
-        <script>
-        document.getElementById('links').onclick = function (event) {
-            event = event || window.event;
-            var target = event.target || event.srcElement,
-                link = target.src ? target.parentNode : target,
-                options = {index: link, event: event},
-                links = this.getElementsByTagName('a');
-            blueimp.Gallery(links, options);
-        };
-        </script>
 	</body>
 </html>
