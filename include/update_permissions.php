f1a871f841967f38347826a5cc39c9ecf20bcae9
LosLosrien <f.siffer@gmail.com>
2014-06-22 21:53:04 +0200

    fixed gitignore, deleted test, permission now work properly
    permission now in twodimensional array. hope it still works...

--- include/update_permissions.php
+++ include/update_permissions.php
@@ -4,19 +4,18 @@ include "configurations.php";
 
 //This file updates the permission File
 
-//Initialization
-$current = date_create();
-$current = date_timestamp_get($current);
-if (!file_exists($pFile)) {
-	file_put_contents($pFile, json_encode([$current]));
+if (file_exists($pFile)) {
+	//Initialization
+	$current = date_create();
+	$current = date_timestamp_get($current);
+	$permissions = json_decode(file_get_contents($pFile));
+	//while more requests clear the first after $permission_free_after
+	while (count($permissions) > 1 && $current - $permissions[0][0] >= $permission_free_after) {
+		unset($permissions[0]);
+		$permissions = array_values($permissions);
+		file_put_contents($pFile, json_encode($permissions));
+	}
 }
-$permissions = json_decode(file_get_contents($pFile));
 
-//If more requests clear the first after $permission_free_after
-if ($current - $permissions[0] >= $permission_free_after && count($permissions) > 2 ) {
-	unset($permissions[1]);
-	$permissions[0] = $current; //this works, however it is not perfect: if should be while and it should be a two dimensional array: each entry both contains the request timestamp and the uuid
-	$permissions = array_values($permissions);
-	file_put_contents($pFile, json_encode($permissions));
-}
+
 ?>
--- lib/get_permission_status.php
+++ lib/get_permission_status.php
@@ -5,23 +5,41 @@ include "../include/update_permissions.php";
 
 //this returns the status of the permission request for a uuid
 
-//Initialization 
-$permissions = json_decode(file_get_contents($pFile));
-$current = date_create();
-$current = date_timestamp_get($current);
+if (file_exists($pFile)) {
 
-//no respones if not in permission-que
-if (in_array($_REQUEST['uuid'], $permissions)) {
-	if (count($permissions)==2){
-		echo('0'); //means uuid has currently controll for a unlimited ammount of time
-	} else {
-		$i = $permission_free_after - ($current-$permissions[0]);
-		if ($permissions[1]===$_REQUEST['uuid']) {
-			echo($i);
+	//Initialization 
+	$current = date_create();
+	$current = date_timestamp_get($current);
+	$permissions = json_decode(file_get_contents($pFile));
+	
+	//no respones if not in permission-que
+	if (in_array_r($_REQUEST['uuid'], $permissions)) {
+		if (count($permissions)==1){
+			echo('0'); //means uuid has currently controll for a unlimited ammount of time
 		} else {
-			$i = $i + (array_search($_REQUEST['uuid'],$permissions)-2)*$permission_free_after;
-			echo('-'.$i);
+			$int = $permission_free_after - ($current-$permissions[0][0]);			
+			if ($permissions[0][1]===$_REQUEST['uuid']) {
+				echo($int); //has controll for this amount of time left
+			} else {
+				for ($i = 1; $i < count($permissions); $i++) {	
+					if ($permissions[$i][1] == $_REQUEST['uuid']) {
+						$int = $int + $i*$permission_free_after;
+						break;
+					}
+				}
+				echo('-'.$int); //gains control in this amount of time
+			}
 		}
 	}
+}
+
+//in_array for multidimensional arrays:
+function in_array_r($needle, $haystack, $strict = false) {
+    foreach ($haystack as $item) {
+        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+            return true;
+        }
+    }
+    return false;
 }
 ?>
