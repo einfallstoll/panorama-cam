5f72a0722324aa3e4373ebbc410863de9b62acb1
LosLosrien <f.siffer@gmail.com>
2014-06-22 21:53:04 +0200

    fixed gitignore, deleted test, permission now work properly
    permission now in twodimensional array. hope it still works...

--- lib/req_permission.php
+++ lib/req_permission.php
@@ -4,8 +4,28 @@ include "../include/configurations.php";
 include "../include/update_permissions.php";
 
 //Request Control for future use
-$permissions = json_decode(file_get_contents($pFile));
-array_push($permissions,$_REQUEST['uuid']);
+
+//Initialization 
+$current = date_create();
+$current = date_timestamp_get($current);
+$permissions = [];
+$afterLast = 0;
+
+//Fetch existing Data
+if (file_exists($pFile)) {
+	$permissions = json_decode(file_get_contents($pFile));
+	if (count($permissions) > 0) {
+		$afterLast = end($permissions)[0] + $permission_free_after;
+	}
+}
+
+//Create request for permission
+$toPush = [$current,$_REQUEST['uuid']];
+if ($afterLast > $current) {
+	$toPush[0] = $afterLast;
+}
+
+//Push&Save
+array_push($permissions,$toPush);
 file_put_contents($pFile, json_encode($permissions));
-echo('1');
 ?>
