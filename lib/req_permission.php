b603f1ac36a0ca33086158ee771667c41358a92f
LosLosrien <f.siffer@gmail.com>
2014-06-22 20:56:04 +0200

    first working version

--- lib/req_permission.php
+++ lib/req_permission.php
@@ -1,8 +1,11 @@
 <?php
-include "include/constants.php";
-include "include/configurations.php";
-include "include/update_permissions.php";
+include "../include/constants.php";
+include "../include/configurations.php";
+include "../include/update_permissions.php";
 
-//Request Control
-file_put_contents($path, $_REQUEST('uuid'), FILE_APPEND);
+//Request Control for future use
+$permissions = json_decode(file_get_contents($pFile));
+array_push($permissions,$_REQUEST['uuid']);
+file_put_contents($pFile, json_encode($permissions));
+echo('1');
 ?>
